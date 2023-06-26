9d501a4715c204f7fc2d20a95830b0058599b261
Guilherme Torquato da Costa <guilhermetor10@gmail.com>
2023-06-25 23:28:24 -0300

    starting to make progress on home page

--- src/app/components/home/home.module.ts
+++ src/app/components/home/home.module.ts
@@ -1,14 +1,19 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+
 import { ButtonModule } from 'primeng/button';
+import { CardModule } from 'primeng/card';
+
 import { HomeComponent } from './home.component';
+import { AppointmentListComponent } from './ui/appointment-list/appointment-list.component';
 
 
 @NgModule({
-  declarations: [HomeComponent],
+  declarations: [HomeComponent, AppointmentListComponent],
   imports: [
     CommonModule,
     ButtonModule,
+    CardModule
   ]
 })
 export class HomeModule { }
